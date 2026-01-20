d4c151c9b051899ccdf9e35ce308414a0e13d7b9
Yeabsirashimelis <shimelisyeabsiragithub@gmail.com>
2026-01-20 23:26:47 +0300

    feat: Add validation attributes to DTOs - Added Required, StringLength, and Range validation attributes

--- GameStore.Api/Dtos/CreateGameDto.cs
+++ GameStore.Api/Dtos/CreateGameDto.cs
@@ -1,8 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace GameStore.Api.Dtos;
 
 public record CreateGameDto(
+    [Required(ErrorMessage = "Name is required")]
+    [StringLength(100, MinimumLength = 1, ErrorMessage = "Name must be between 1 and 100 characters")]
     string Name,
+
+    [Required(ErrorMessage = "Genre is required")]
+    [StringLength(50, MinimumLength = 1, ErrorMessage = "Genre must be between 1 and 50 characters")]
     string Genre,
+
+    [Range(0.01, 999.99, ErrorMessage = "Price must be between $0.01 and $999.99")]
     decimal Price,
+
+    [Required(ErrorMessage = "Release date is required")]
     DateOnly ReleaseDate
 );
